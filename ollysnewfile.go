829a31d8b861f547a1b18dabbbb8cb8adbc35fd7
Oliver Filmer <ollyfilmer@yahoo.co.uk>
2024-01-14 23:23:01 +0000

    new changes

---
+++ ollysnewfile.go
@@ -0,0 +1,7 @@
+package main
+
+// myth: Added new feature to integrate QueenE's twitch stream chat functionality into our service. Allowing top quality entertainment for our customers.
+
+// myth: Security overhaul, moved to using Azure Active Directory.
+
+// myth: New sms provider, twillio.
