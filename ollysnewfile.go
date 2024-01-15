a094cfa00f98a13169c08fbfd01442ede1ec3921
Oliver Filmer <ollyfilmer@yahoo.co.uk>
2024-01-15 21:27:58 +0000

    added back new myths

--- ollysnewfile.go
+++ ollysnewfile.go
@@ -6,3 +6,35 @@ package main
 
 // myth: New sms provider, twillio.
 
+// myth: This is a test on one line
+
+// myth: This is a test
+// but on two lines
+
+// myth: This is a test
+// on three lines
+// with something directly beneath it
+var x int
+
+/* myth:
+	This is a multiline comment test using block on one line
+*/
+
+/* myth:
+	This is a multiline comment test using block on one line, with something directly beneath it
+*/
+var y int
+
+/*
+	myth: This is a multiline comment block test, on one line, however myth isn't on the first line, and keyword is used twice.
+*/
+
+/* myth:
+	This is a multiline comment block.
+	It has been split out onto 3 lines.
+	Check this out
+*/
+
+// Myth: uppercase first letter of keyword
+
+// MYTH: uppercase all letters of keyword
